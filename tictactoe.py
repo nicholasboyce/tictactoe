ad819de24aa8ef4661b1968a478ac314bf23596d
nicholasboyce <nicholasboyce@protonmail.com>
2024-10-26 20:20:36 -0400

    Created skeleton for Board mark fxn

--- tictactoe.py
+++ tictactoe.py
@@ -8,11 +8,20 @@ class TicTacToe:
         The first player is always X.
         """
         self._board = self.Board()
-    
 
     class Board:
+        """A 3x3 board for tic-tac-toe."""
 
         def __init__(self):
+            """Create a new game board.
+
+            state: a multidimensional array representation of the current board
+            """
             self.state = [[0] * 3 for i in range(3)]
         
-        def mark()
+        def mark(position: list, player: str) -> bool:
+            """Marks a given position on the board with the player's corresponding symbol.
+
+            Returns True if the given position is valid and successfully marked. Otherwise, returns False.
+            """
+            return True
