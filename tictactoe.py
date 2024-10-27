c1a358cbdeb8ba9cf62ea5a03736e696803a517b
nicholasboyce <nicholasboyce@protonmail.com>
2024-10-26 21:33:35 -0400

    Added Player inner class.

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,39 @@
 class TicTacToe:
     """A tic-tac-toe game."""
 
-    def __init__(self):
+    def __init__(self, computer: bool, cpuPlayer: int):
         """Create a new game instance.
 
         The initial board created is empty.
         The first player is always X.
         """
         self._board = self.Board()
+        self._players = self._determine_players(computer, cpuPlayer)
+        self._curr_player = 0 #Corresponds to X
+        self._finished = False
+
+    def _determine_players(self, computer: bool, cpuPlayer: int) -> list:
+        x, y = None
+        if computer:
+            if cpuPlayer - 1: #i.e. player X is the computer
+                x = self.Player('X', computer)
+                y = self.Player('Y', not computer)
+            else:
+                x = self.Player('X', not computer)
+                y = self.Player('Y', computer)
+        else: #neither is computer
+            x = self.Player('X', not computer)
+            y = self.Player('Y', not computer)
+        return [x, y]
+
+    def play_round(self) -> None:
+        return None
+    
+    class Player:
+
+        def __init__(self, symbol: str, computer: bool):
+            self.symbol = symbol
+            self.computer = computer
 
     class Board:
         """A 3x3 board for tic-tac-toe."""
@@ -19,7 +45,7 @@ class TicTacToe:
             """
             self.state = [[0] * 3 for i in range(3)]
         
-        def mark(position: list, player: str) -> bool:
+        def mark(self, position: list, player: str) -> bool:
             """Marks a given position on the board with the player's corresponding symbol.
 
             Returns True if the given position is valid and successfully marked. Otherwise, returns False.
